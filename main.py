7983c16b674a6e2582ab80faf8d01e62854255d0
eric-anderson-eja <eric.anderson.eja@gmail.com>
2025-12-13 19:16:57 -0600

    end of chapter 1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from google.genai import types
 
 
 
-# ***** Configuration *****
+# *****   Configuration   *****
 load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
 MODEL_ID = 'gemini-2.5-flash'
@@ -23,23 +23,25 @@ client = genai.Client(api_key=API_KEY)
 # *****  Input Handling  *****
 parser = argparse.ArgumentParser(description="ai_bot")
 parser.add_argument("user_prompt", type=str, help="Please enter prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 user_prompt = args.user_prompt
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-# ****  Core Logic  ***
+
+
+# *****  Core Logic  *****
 response = client.models.generate_content(
     model='gemini-2.5-flash', 
     contents=messages
     )
 
-#  ***** Validation and Output *****
+# *****  Validation and Output   *****
 if response.usage_metadata == 'None':
     raise RuntimeError("POSSIBLE FAILED REQUEST, no token metadata")
-
-print(f"User prompt: {user_prompt}")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if args.verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 print(f"Response:\n{response.text}")
-
